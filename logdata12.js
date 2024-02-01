ada6704045b533cced1d753778d092edb0193cae
ian <20951604+iansecretario@users.noreply.github.com>
2024-02-01 22:53:38 +0800

    Create logdata12.js

---
+++ logdata12.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+    var courseData = {};
+
+    function addClipboardButton() {
+        $('pre').each(function() {
+            var $pre = $(this);
+            var $clipboardButton = $('<button class="btn btn-secondary btn-sm clipboard-button" data-clipboard-target="#' + $pre.attr('id') + '">Copy</button>');
+            $pre.addClass('position-relative');
+            $clipboardButton.appendTo($pre);
+        });
+    }
+
+    function onContentChange(data) {
+        var lastActivityDate = new Date().toISOString();
+
+        courseData = {
+            courseId: data.course.id,
+            courseName: data.course.name,
+            courseSlug: data.course.slug,
+            lessonId: data.lesson.id,
+            lessonName: data.lesson.name,
+            lessonSlug: data.lesson.slug,
+            chapterName: data.chapter.name,
+            chapterId: data.chapter.id,
+            userId: data.user.id,
+            userName: data.user.full_name,
+            userFirstName: data.user.first_name,
+            userEmail: data.user.email,
+            lastActivityDate: lastActivityDate
+        };
+
+        console.log('Course Data:', courseData);
+
+        var requestData = {
+            userId: courseData.userId,
+            courseId: courseData.courseId,
+            chapterId: courseData.chapterId,
+            lessonId: courseData.lessonId
+        };
+
+        $.ajax({
+            url: 'https://sb1.guidem.ph/checkuserprogress',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(requestData),
+            success: function(response) {
+                console.log('User progress data:', response);
+
+                if (response && response.completedQuestions) {
+                    var completedQuestions = response.completedQuestions.map(String);  // Convert each element to a string
+                    console.log('Completed questions:', completedQuestions);
+
+                    $('.guidem-form').each(function() {
+                        var $form = $(this);
+                        var questionNumber = $form.data('question-id').toString(); // Convert to string for comparison
+
+                        if (completedQuestions.includes(questionNumber)) {
+                            $form.find('input[type="text"]').prop('disabled', true)
+                                .attr('placeholder', 'You already completed this. Grit & Grind!');
+                            $form.find('.guidem-button').text('Completed')
+                                .css({'background-color': '#218838', 'color': 'white'})
+                                .prop('disabled', true);
+                            $form.find('.guidem-hint-button').prop('disabled', true)
+                                .css('background-color', 'gray');
+                        }
+                    });
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error('Error:', error);
+            }
+        });
+    }
+
+    if (typeof(CoursePlayerV2) !== 'undefined') {
+        CoursePlayerV2.on('hooks:contentDidChange', onContentChange);
+    }
+
+    addClipboardButton();
+});
